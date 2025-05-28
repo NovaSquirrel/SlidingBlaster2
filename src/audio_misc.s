05d160b218550750723f7ee25793a7aa847c9e91
NovaSquirrel <bushytail@novasquirrel.com>
2025-05-27 23:55:24 -0400

    Update Terrific Audio Driver

--- src/audio_misc.s
+++ src/audio_misc.s
@@ -1,19 +1,24 @@
-; Sliding Blaster 2
-; Copyright (C) 2025 NovaSquirrel
+; SNES platformer example
 ;
-; This program is free software: you can redistribute it and/or
-; modify it under the terms of the GNU General Public License as
-; published by the Free Software Foundation; either version 3 of the
-; License, or (at your option) any later version.
+; Copyright (c) 2022 NovaSquirrel
 ;
-; This program is distributed in the hope that it will be useful, but
-; WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-; General Public License for more details.
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
 ;
-; You should have received a copy of the GNU General Public License
-; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
 ;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
 
 .include "snes.inc"
 .include "global.inc"
@@ -88,7 +93,6 @@ LoadCommon:
 
 .export Tad_Loader_Bin, Tad_Loader_SIZE
 .export Tad_AudioDriver_Bin, Tad_AudioDriver_SIZE
-.export Tad_BlankSong_Bin, Tad_BlankSong_SIZE
 
 CommonAudioData_Bin: .incbin "../audio/audio_common.bin"
 CommonAudioData_SIZE = .sizeof(CommonAudioData_Bin) 
@@ -98,6 +102,3 @@ Tad_Loader_SIZE = .sizeof(Tad_Loader_Bin)
 
 Tad_AudioDriver_Bin: .incbin "../audio/driver/audio-driver.bin"
 Tad_AudioDriver_SIZE = .sizeof(Tad_AudioDriver_Bin)
-
-Tad_BlankSong_Bin: .incbin "../audio/driver/blank-song.bin"
-Tad_BlankSong_SIZE = .sizeof(Tad_BlankSong_Bin)
